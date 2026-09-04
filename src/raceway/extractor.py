0b8b687c556d1061b30a9b70955dcdaa783746a9
Ian Wilson <ian@gardentheory.com>
2026-09-04 00:08:23 -0700

    Add extractor for scraping dep specs off factories.

---
+++ src/raceway/extractor.py
@@ -0,0 +1,108 @@
+from annotationlib import Format, call_evaluate_function
+from collections.abc import Callable
+from dataclasses import dataclass, fields, is_dataclass, field, Field, replace
+from inspect import isclass
+from typing import (
+    Any,
+    get_origin,
+    get_args,
+    Annotated,
+    get_type_hints,
+    Mapping,
+    Protocol,
+    TypeAliasType,
+    is_protocol,
+)
+
+from .exc import RacewayError
+from .protocols import IServiceFactory, IMarker
+from .registration import DepSpec
+
+
+class ExtractionError(RacewayError):
+    pass
+
+
+@dataclass
+class Cabled:
+    """
+    Mark a dependency as "Cabled" to have it injected.
+
+    example
+
+    @configure_as_service
+    class ImageService:
+        db: Annotated[ISession, Cabled] # class spec
+        file_path: Annotated[str, Cabled(IConfig, key='config.file_path')] # spec instance with proto and custom kwargs
+        fs_api: IFileSystem # without spec
+
+    """
+    proto: IMarker | None = None
+    """Protocol that should be used. If None then try to use type hint as protocol."""
+
+    attr: str | None = None
+    key: str | None = None
+    call_args: tuple | None = None
+    call_kwargs: tuple[tuple[str, object], ...] | None = None
+
+    def to_dep_spec(self) -> DepSpec:
+        if self.proto is None:
+            raise ExtractionError('Cannot generate DepSpec without protocol.')
+        return DepSpec(proto=self.proto, attr=self.attr, key=self.key, call_args=self.call_args, call_kwargs=self.call_kwargs)
+
+
+@dataclass
+class Extractor:
+    """ Extractor dependency specs from annotations. """
+
+    can_resolve_without_spec: Callable[[object], bool] | None = None
+    """ User could provide this as a factory kwargs during decoration. """
+
+    def _can_resolve_without_spec(self, proto: object) -> bool:
+        """ We only resolve protocols "automatically" right now. """
+        if self.can_resolve_without_spec is not None:
+            return self.can_resolve_without_spec(proto)
+        else:
+            return is_protocol(proto)
+
+    def extract(
+        self,
+        service_factory: IServiceFactory,
+    ) -> tuple[tuple[str, DepSpec], ...]:
+        """
+        Extract any dependency specifications we find.
+        """
+        cables = []
+        hints = get_type_hints(service_factory, include_extras=True)
+        for k, hint in hints.items():
+
+            # type MyCabledProto = Annotated[Proto, Cabled]
+            if isinstance(hint, TypeAliasType):
+                hint = call_evaluate_function(hint.evaluate_value, Format.VALUE)
+
+            origin = get_origin(hint)
+            if origin is Annotated:
+                a_args = get_args(hint)
+                proto = a_args[0]
+                for a_arg in a_args[1:]:
+                    spec: Cabled | None = None
+                    if isinstance(a_arg, Cabled):
+                        # ie. Annotated[Proto, Cabled(...)] or Annotated[Proto, Cabled()]
+                        spec = a_arg
+                    elif isclass(a_arg) and issubclass(a_arg, Cabled):
+                        # ie. Annotated[Proto, Cabled]
+                        spec = Cabled()
+
+                    if spec is not None:
+                        if spec.proto is None:
+                            # Use hint type if spec does not define one.
+                            spec = replace(spec, proto=proto)
+                        cables.append((k, spec))
+                    elif self._can_resolve_without_spec(proto):
+                        cables.append((k, Cabled(proto=proto)))
+            # @TODO: I don't think this check is completely accurate.
+            else:
+                proto = hint
+                if proto and self._can_resolve_without_spec(proto):
+                    cables.append((k, Cabled(proto=proto)))
+        return tuple((name, cable.to_dep_spec()) for name, cable in cables if cable.proto is not None)
---
+++ tests/test_extractor.py
@@ -0,0 +1,65 @@
+from dataclasses import dataclass
+from pathlib import Path
+import pytest
+from typing import Protocol, Annotated
+
+
+from raceway.extractor import Extractor, Cabled
+
+
+class IFile(Protocol):
+    pass
+
+
+type CabledFileService = Annotated[IFile, Cabled]
+
+
+@dataclass
+class PatternService:
+
+    file0_api: IFile
+    """ No cabled example, passes can_* check. """
+
+    file1_api: Annotated[IFile, Cabled]
+    """ As class example. """
+
+    file2_api: Annotated[IFile, Cabled()]
+    """ As empty instance example. """
+
+    file3_api: Annotated[str, Cabled(IFile)]
+    """ As instance with proto example. """
+
+    file4_api: CabledFileService
+    """ As type alias example. """
+
+
+@dataclass
+class ParamsService:
+
+    file_params_api: Annotated[IFile, Cabled(key="test_key", attr="test_attr", call_args=("test_call_arg",), call_kwargs=(("test_call_kwarg", 1),))]
+    """ Kitchen sink of various configuration params. """
+
+
+class TestExtract:
+
+    @pytest.fixture
+    def default_ex(self):
+        # Used to be a tractor but its not anymore.
+        return Extractor()
+
+    def test_cabled_patterns(self, default_ex):
+        dep_specs = default_ex.extract(PatternService)
+        lookup = dict(dep_specs)
+        for i in range(5):
+            k = f'file{i}_api'
+            assert k in lookup
+            assert lookup[k].proto is IFile
+
+    def test_cabled_params(self, default_ex):
+        dep_specs = default_ex.extract(ParamsService)
+        lookup = dict(dep_specs)
+        assert "file_params_api" in lookup
+        assert lookup["file_params_api"].key == "test_key"
+        assert lookup["file_params_api"].attr == "test_attr"
+        assert lookup["file_params_api"].call_args[0] == "test_call_arg"
+        assert lookup["file_params_api"].call_kwargs[0][0] == "test_call_kwarg" and lookup["file_params_api"].call_kwargs[0][1] == 1
