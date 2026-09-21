ad0fa2f695c1c8ea78a28dd20e2e95e98eecfd18
Ian Wilson <ian@gardentheory.com>
2026-09-21 16:12:32 -0700

    Add cache warming feature. (#3)
    * Add cache warming feature.

--- src/raceway/registry.py
+++ src/raceway/registry.py
@@ -1,12 +1,10 @@
 from dataclasses import dataclass
 from types import MappingProxyType
 
-from .protocols import IRegistration, IRegistry
-
-type RegTupleType[T] = tuple[type[T], IRegistration[T]]
+from .protocols import IRegistration, IRegistry, ScopeType, RegEntryType
 
 
-def configure_registry(registrations: tuple[RegTupleType, ...]) -> IRegistry:
+def configure_registry(registrations: tuple[RegEntryType, ...]) -> IRegistry:
     return Registry(registrations=MappingProxyType(dict(registrations)))
 
 
@@ -17,3 +15,11 @@ class Registry(IRegistry):
 
     def find[T](self, proto: type[T]) -> IRegistration[T] | None:
         return self.registrations.get(proto)
+
+    def get_entries_by_scope(
+        self,
+        scope: ScopeType,
+    ) -> tuple[RegEntryType, ...]:
+        return tuple(
+            [(k, v) for (k, v) in self.registrations.items() if v.scope == scope]
+        )
